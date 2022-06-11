949503b61af84babc97a320716ce9e1d036d893b
ysl-28 <88529907+ysl-28@users.noreply.github.com>
2022-06-11 22:03:36 +0800

    Added moving platform with circular motion

---
+++ Assets/Scripts/CircularMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularMovement : MonoBehaviour
+{
+   [SerializeField]
+   Transform rotationCenter; 
+
+   [SerializeField]
+   float rotationRadius = 2f;
+
+   [SerializeField]
+   float angularSpeed = 2f;
+
+   float posX = 0f;
+   float posY = 0f;
+   float angle = 0f;
+
+   void Update()
+   {
+    posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
+    posY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
+    transform.position = new Vector2(posX, posY);
+    angle = angle + Time.deltaTime * angularSpeed;
+
+    if (angle >= 360f)
+    {
+        angle = 0f;
+    }
+   }
+}
