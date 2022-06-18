724d4d6ea7313469769b1acd3f4e05bd1f7f5c57
ysl-28 <88529907+ysl-28@users.noreply.github.com>
2022-06-18 21:12:56 +0800

    Added environment layer

---
+++ Assets/Scripts/GlowBallController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowBallController : MonoBehaviour
+{
+    private bool isContact = false;
+    Animator myAnim;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+        myAnim.SetBool("isContact", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if(other.gameObject.tag == "Player") 
+        {
+            isContact = true;
+            myAnim.SetBool("isContact", true);
+            Debug.Log("after isContact was set to true");
+            
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerEXIT called");
+        if(other.gameObject.tag == "Player")
+        {
+           isContact = false;
+           myAnim.SetBool("isContact", false);
+           Debug.Log("after isContact was set to false");
+        }
+        
+    }
+}
