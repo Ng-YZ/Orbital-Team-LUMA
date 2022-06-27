5d5cb324ef80a02c894de49b9a98e4c9656d221d
ysl-28 <88529907+ysl-28@users.noreply.github.com>
2022-06-27 12:05:49 +0800

    Fixed issue causing enemies to give double points when destroyed

--- Assets/Scripts/PlayerTeleport.cs
+++ Assets/Scripts/PlayerTeleport.cs
@@ -15,7 +15,7 @@ public class PlayerTeleport : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
             if (currentTeleporter != null)
             {
