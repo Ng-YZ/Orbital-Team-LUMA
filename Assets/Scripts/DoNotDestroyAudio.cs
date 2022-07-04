dae7dc616ba0ac914eb0017bc49bde01f95a9710
Ng-YZ <e0772909@u.nus.edu>
2022-07-05 00:25:32 +0800

    Change audio to pause/continue in pause menu

--- Assets/Scripts/DoNotDestroyAudio.cs
+++ Assets/Scripts/DoNotDestroyAudio.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoNotDestroyAudio : MonoBehaviour
-{
+{   
     private void Awake() {
         GameObject[] musicObj = GameObject.FindGameObjectsWithTag("Music");
         if(musicObj.Length > 1) 
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -23,24 +23,28 @@ public class PauseMenu : MonoBehaviour
 
     public void Resume() {
         pauseMenuUI.SetActive(false);
+        AudioListener.pause = false;
         Time.timeScale = 1f;
         GamePaused = false;
-        MusicPlayerScript.objectMusic.GetComponent<AudioSource>().Play();
     }
 
     void Pause() {
         pauseMenuUI.SetActive(true);
+        AudioListener.pause = true;
         Time.timeScale = 0f;
         GamePaused = true;
-        MusicPlayerScript.objectMusic.GetComponent<AudioSource>().Pause();
     }
 
     public void LoadMenu() {
+        SceneManager.LoadScene(sceneName: "Options");
+        Time.timeScale = 1f;
+        AudioListener.pause = false;
         Debug.Log("Loading Menu.");
     }
 
     public void QuitToMenu() {
         Time.timeScale = 1f;
+        AudioListener.pause = false;
         SceneManager.LoadScene(sceneName:"Main Menu");
     }
 }
