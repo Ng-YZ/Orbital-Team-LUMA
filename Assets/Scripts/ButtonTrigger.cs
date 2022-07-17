1d8ee828d9a74598adba46d4e5bd862c581fbf00
ysl-28 <88529907+ysl-28@users.noreply.github.com>
2022-07-18 00:46:54 +0800

    Add layout of level 4

---
+++ Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour
+
+{
+    
+    
+    Animator ButtonPress;
+    //private int BlockIndex = 0;
+
+    [SerializeField] private GameObject[] Blocks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ButtonPress = GetComponent<Animator>();
+        ButtonPress.SetBool("isContact", false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player") 
+        {
+
+            ButtonPress.SetBool("isContact", true);
+            //Blocks[].SetActive(false);
+
+            foreach(GameObject block in Blocks) 
+            {
+                block.SetActive(false);
+            }
+            
+        }
+    }
+}
