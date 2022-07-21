528445828fb4a7ef2a0b50e8f30b5404cdc9ddd7
ysl-28 <88529907+ysl-28@users.noreply.github.com>
2022-07-22 00:05:35 +0800

    Adjust platform heights in level 4 and add instance of score deduction

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Respawn : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
 
+    public PlayerInteraction playerInteract;
+
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Player")
         {
+            if (playerInteract.score < 400) 
+            {
+                playerInteract.score = 0;
+            }
+
+            else 
+            {
+                playerInteract.score -= 400;
+            }
+        
+       
+       
+       
+       
+        Debug.Log(playerInteract.score);
+
+        playerInteract.scoreText.text = playerInteract.score.ToString();
+
         player.transform.position = respawnPoint.transform.position;
         }
         
